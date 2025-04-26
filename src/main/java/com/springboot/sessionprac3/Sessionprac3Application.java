4f1861f65e12588387cc05de38546abab8f2c24d
jinyoung02 <jink8962@naver.com>
2025-04-26 14:32:46 +0900

    init

---
+++ src/main/java/com/springboot/sessionprac3/Sessionprac3Application.java
@@ -0,0 +1,13 @@
+package com.springboot.sessionprac3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sessionprac3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sessionprac3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/sessionprac3/Sessionprac3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.sessionprac3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sessionprac3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
