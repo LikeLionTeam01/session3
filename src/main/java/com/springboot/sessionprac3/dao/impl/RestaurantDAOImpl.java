f52fccad9095868b983fc7549b22d0734981f5bf
jinyoung02 <jink8962@naver.com>
2025-04-28 17:28:36 +0900

    commit 1

---
+++ src/main/java/com/springboot/sessionprac3/dao/impl/RestaurantDAOImpl.java
@@ -0,0 +1,11 @@
+package com.springboot.sessionprac3.dao.impl;
+
+import com.springboot.sessionprac3.dao.RestaurantDAO;
+import com.springboot.sessionprac3.repository.RestaurantRepository;
+
+public class RestaurantDAOImpl {
+
+    private RestaurantDAOImpl(RestaurantRepository restaurantRepository) {
+
+    }
+}
